d383ccf99d51ecad71f64e851cc9229def7fc409
Fiiranek <jellybean@onet.eu>
2025-06-23 14:30:40 +0200

    More logs

--- dist/index.js
+++ dist/index.js
@@ -29996,6 +29996,8 @@ async function findStarknetFoundryDir(extractedPath) {
 
 async function main() {
   try {
+    console.log(core.getInput("starknet-foundry-version"));
+    console.log(core.getInput("tool-versions"));
     const StarknetFoundryVersionInput = core.getInput(
       "starknet-foundry-version",
     );
