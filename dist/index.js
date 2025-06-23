fc10d701996093653df6db5513811b2c92f666fe
Fiiranek <jellybean@onet.eu>
2025-06-23 16:03:02 +0200

    Code cleanup

--- dist/index.js
+++ dist/index.js
@@ -29996,11 +29996,9 @@ async function findStarknetFoundryDir(extractedPath) {
 
 async function main() {
   try {
-    // const StarknetFoundryVersionInput = core.getInput(
-    //   "starknet-foundry-version",
-    // );
-    const StarknetFoundryVersionInput =
-      "${{ inputs.starknet-foundry-version }}";
+    const StarknetFoundryVersionInput = core.getInput(
+      "starknet-foundry-version",
+    );
 
     const toolVersionsPathInput = core.getInput("tool-versions");
 
